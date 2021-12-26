28f2d41fec5b2f6b8a259de961ff5efbd144a9b1
StonedEdge <45709395+StonedEdge@users.noreply.github.com>
2021-12-26 18:06:57 +0900

    Update RetroLiteMonitorScript.cpp

--- ATtiny84/RetroLiteMonitorScript.cpp
+++ ATtiny84/RetroLiteMonitorScript.cpp
@@ -84,3 +84,75 @@ void powerButtonDebounce(){
     powerBtnState = input; //Set powerBtnState to 1 if button is pressed, 0 if released/depressed
   }
 }
+
+void BQ_Write(uint8_t address, uint8_t reg, const uint8_t message) {
+  i2c_start_wait(address | I2C_WRITE);
+  i2c_write(reg);
+  i2c_write(message);
+  i2c_stop();
+}
+
+ uint8_t BQ_Read(uint8_t address, uint8_t reg){
+  i2c_start_wait(address | I2C_WRITE);
+  i2c_write(reg);
+  i2c_rep_start(address | I2C_READ);
+  uint8_t data = i2c_read(true);
+  i2c_stop();
+  return(data);
+ }
+
+// BQ24292i Register Configuration
+//
+//// REG00
+//const uint8_t EN_HIZ = 0;
+//const uint8_t EN_ILIM = 1;
+//const uint8_t INILIM = 0b111;
+//const uint8_t INILIM = 0b111;   // 3A
+//
+//// REG01
+//const uint8_t REG_RST = 0;
+//const uint8_t WD_RST = 0;
+//const uint8_t CHG_CONFIG = 0b01;
+//const uint8_t SYS_MIN = 0b011;   // 3.3V
+//const uint8_t BOOST_LIM = 1;
+//
+//// REG02
+//const uint8_t ICHG = 0b011000; // 1.5A
+//const uint8_t FORCE_20PCT = 0;
+//
+//// REG03 default values are OK
+//
+//// REG04
+//const uint8_t VREG = 0b101100; // 4.208V
+//const uint8_t BATLOWV = 0;
+//const uint8_t VRECHG = 0;
+//
+//// REG05
+//const uint8_t EN_TERM = 1;
+//const uint8_t TERM_STAT = 0;
+//const uint8_t WATCHDOG = 0b00; // Disable I2C WD
+//const uint8_t EN_TIMER = 1;
+//const uint8_t CHG_TIMER = 0b01;
+//
+////REG06
+//const uint8_t BAT_COMP = 0;
+//const uint8_t VCLAMP = 0;
+//const uint8_t TREG = 0b01;   // 80C
+
+uint8_t REG00_config = 0b01111111;
+uint8_t REG01_config = 0b00010001;
+uint8_t REG02_config = 0b01000000;
+uint8_t REG04_config = 0b10110000;
+uint8_t REG05_config = 0b10001010;
+uint8_t REG06_config = 0b00000001;
+uint8_t REG07_config = 0b01101011;
+
+void BQ_INIT() {
+    BQ_Write(BQ24292i_ADDRESS, 0x00, REG00_config); 
+    BQ_Write(BQ24292i_ADDRESS, 0x01, REG01_config); 
+    BQ_Write(BQ24292i_ADDRESS, 0x02, REG02_config); 
+    BQ_Write(BQ24292i_ADDRESS, 0x04, REG04_config);
+    BQ_Write(BQ24292i_ADDRESS, 0x05, REG05_config); 
+    BQ_Write(BQ24292i_ADDRESS, 0x06, REG06_config);
+    BQ_Write(BQ24292i_ADDRESS, 0x07, REG07_config);
+}
